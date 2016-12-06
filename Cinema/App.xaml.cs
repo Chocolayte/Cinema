e930f993d312678c54efbb95fe9ebc8c53dd573f
Chocolayte <ludovic.baudin@utbm.fr>
2016-12-06 11:53:09 +0100

    Initialize project

---
+++ Cinema/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Cinema
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            base.OnStartup(e);
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+    }
+}
---
+++ Cinema/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Cinema
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+            if (!Directory.Exists(dialog.SelectedPath))
+                return;
+
+            var exts = new[] { "avi", "mkv", "mp4" };
+            
+            var files = Directory.EnumerateFiles(dialog.SelectedPath, "*.*")
+                                 .Where(file => exts.Any(x => file.EndsWith(x, StringComparison.OrdinalIgnoreCase)) && file != "." && file != "..");
+
+
+        }
+    }
+}
